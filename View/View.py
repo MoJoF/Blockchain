220060db72550d638df2ea8664e883a7c7c84553
MoJoF <mojo.13081998@gmail.com>
2020-09-10 23:17:55 +0300

    Продолжаю пилить UI

--- View/View.py
+++ View/View.py
@@ -9,11 +9,18 @@ class View:
 		self.frame1 = Frame(self.root, height=300, width=300, bg='DarkSlateBlue') # В данном фрейме будут кнопки отправки и получения средств на кошелек
 		self.frame1.pack(anchor=N)
 
+		self.button_send = Button(self.frame1, text='Отправить', bg='MediumOrchid', fg='white', command=self.send())
+		self.button_send.pack(anchor=NW)
+
 		self.frame2 = Frame(self.root, height=200, width=300, bg='SlateBlue')
 		self.frame2.pack(anchor=S)
 
 
 		
-		
+
 
 		self.root.mainloop()
+
+
+	def send(self):
+		pass
