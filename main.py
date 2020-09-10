15b27b30ea49cc08d63c1a58390ada48d91bd673
MoJoF <mojo.13081998@gmail.com>
2020-09-10 22:16:00 +0300

    Исправление ошибки в классе. Доработка генерирования хэша.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+
 class Block:
-	def __init__(adress_send, adress_get, count, time, hash_key, prev_hash_key):
+	def __init__(self, adress_send, adress_get, count, time, hash_key, prev_hash_key):
 		self.adress_send = adress_send
 		self.adress_get = adress_get
 		self.count = count
@@ -8,11 +10,18 @@ class Block:
 		self.prev_hash_key = prev_hash_key
 
 	def hash_generate(self):
-		self.arr = 'abcdefghijklmnopqrstuvwxyz'
-		self.arr = self.arr + self.arr.upper() + '1234567890'
+		self.arr = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, '
+		self.arr = self.arr + self.arr.upper() + '1, 2, 3, 4, 5, 6, 7, 8, 9, 0'
+		self.arr = self.arr.split(',')
 
-		return self.arr
+		self.res = []
+
+		for i in range(34):
+			self.res.append(random.choice(self.arr))
+			# "".join(self.res)
+
+		print("".join(self.res).strip(' '))
 
 
-a = Block(0,0,0,0,0)
+a = Block(0,0,0,0,0,0)
 a.hash_generate()
