b2333acbdb3f5504611a38416d69cd571b8a47a7
MoJoF <mojo.13081998@gmail.com>
2020-09-10 22:20:51 +0300

    Полностью допилен механизм генерации хэша

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ class Block:
 		self.prev_hash_key = prev_hash_key
 
 	def hash_generate(self):
-		self.arr = 'a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, '
-		self.arr = self.arr + self.arr.upper() + '1, 2, 3, 4, 5, 6, 7, 8, 9, 0'
+		self.arr = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z, '
+		self.arr = self.arr + self.arr.upper() + '1,2,3,4,5,6,7,8,9,0'
 		self.arr = self.arr.split(',')
 
 		self.res = []
 
-		for i in range(34):
+		for i in range(70):
 			self.res.append(random.choice(self.arr))
-			# "".join(self.res)
 
-		print("".join(self.res).strip(' '))
+		self.res = "".join(self.res).replace(" ", "")
+
+		return self.res
+
+	
+
 
 
 a = Block(0,0,0,0,0,0)
-a.hash_generate()
+print(a.hash_generate())
