2928142c940f25be4f8442e65c8f16e467640983
MoJoF <mojo.13081998@gmail.com>
2020-09-10 23:06:03 +0300

    Начало разработки графического интерфейса

--- View/View.py
+++ View/View.py
@@ -6,6 +6,14 @@ class View:
 		self.root.title("Levendauer Corp")
 		self.root.geometry("300x500+320+200")
 
-		self.frame1 = Frame() # В данном фрейме будут кнопки отправки и получения средств на кошелек
+		self.frame1 = Frame(self.root, height=300, width=300, bg='DarkSlateBlue') # В данном фрейме будут кнопки отправки и получения средств на кошелек
+		self.frame1.pack(anchor=N)
+
+		self.frame2 = Frame(self.root, height=200, width=300, bg='SlateBlue')
+		self.frame2.pack(anchor=S)
+
+
+		
+		
 
 		self.root.mainloop()
